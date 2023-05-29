65940c2465c303093bcab2348ca76c63df45b465
Chhakuli <chhakulizingare12@gmail.com>
2023-05-29 07:52:18 +0530

    Initialize project using Create React App

---
+++ src/backend/db/users.js
@@ -0,0 +1,18 @@
+import { v4 as uuid } from "uuid";
+import { formatDate } from "../utils/authUtils";
+/**
+ * User Database can be added here.
+ * You can add default users of your wish with different attributes
+ * */
+
+export const users = [
+  {
+    _id: uuid(),
+    firstName: "Adarsh",
+    lastName: "Balika",
+    username: "adarshbalika",
+    password: "adarshBalika123",
+    createdAt: formatDate(),
+    updatedAt: formatDate(),
+  },
+];
