b8cbf69f46349e8b170b96a3445c8e2d8b0f9e80
Chhakuli <chhakulizingare12@gmail.com>
2023-05-30 10:05:05 +0530

    feat: Add metarial icons

---
+++ src/asset/icons/metarialIcons.js
@@ -0,0 +1,30 @@
+import DarkModeOutlinedIcon from "@mui/icons-material/DarkModeOutlined";
+import LightModeOutlinedIcon from "@mui/icons-material/LightModeOutlined";
+import HomeOutlinedIcon from "@mui/icons-material/HomeOutlined";
+import BookmarkBorderOutlinedIcon from "@mui/icons-material/BookmarkBorderOutlined";
+import LogoutIcon from "@mui/icons-material/Logout";
+import PostAddIcon from "@mui/icons-material/PostAdd";
+import RocketOutlinedIcon from "@mui/icons-material/RocketOutlined";
+import FavoriteBorderOutlinedIcon from "@mui/icons-material/FavoriteBorderOutlined";
+import AddCircleOutlineOutlinedIcon from "@mui/icons-material/AddCircleOutlineOutlined";
+import SearchOutlinedIcon from "@mui/icons-material/SearchOutlined";
+import WhatshotRoundedIcon from "@mui/icons-material/WhatshotRounded";
+import NewReleasesRoundedIcon from "@mui/icons-material/NewReleasesRounded";
+import GitHubIcon from "@mui/icons-material/GitHub";
+import HistoryEduIcon from "@mui/icons-material/HistoryEdu";
+export {
+  DarkModeOutlinedIcon,
+  LightModeOutlinedIcon,
+  HomeOutlinedIcon,
+  BookmarkBorderOutlinedIcon,
+  LogoutIcon,
+  PostAddIcon,
+  RocketOutlinedIcon,
+  FavoriteBorderOutlinedIcon,
+  AddCircleOutlineOutlinedIcon,
+  SearchOutlinedIcon,
+  NewReleasesRoundedIcon,
+  WhatshotRoundedIcon,
+  GitHubIcon,
+  HistoryEduIcon,
+};
