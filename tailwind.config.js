04fb3aa390329b3c9bf63407e6e4a5533905b312
Chhakuli <chhakulizingare12@gmail.com>
2023-05-30 09:53:31 +0530

    feat: Add tailwind config

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+module.exports = {
+  content: ["./src/**/*.{js,jsx}"],
+  theme: {
+    extend: {
+      colors: {
+        customBg: "#FAF9F9",
+        deepBlue: "#253053",
+        customOrange: "#FF6C10",
+      },
+    },
+  },
+  plugins: [],
+};
