1a456b9cf26687365b9c9bf71b1270292cdd3a4a
Chhakuli <chhakulizingare12@gmail.com>
2023-06-01 07:34:29 +0530

    feat: Dispatch action for logout

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,3 +1,9 @@
+import React from "react";
+import { useDispatch } from "react-redux";
+import { useNavigate } from "react-router";
+import { NavLink } from "react-router-dom";
+import { toast } from "react-hot-toast";
+
 import {
   AddCircleOutlineOutlinedIcon,
   BookmarkBorderOutlinedIcon,
@@ -7,16 +13,18 @@ import {
   LogoutIcon,
   RocketOutlinedIcon,
 } from "asset";
-import React from "react";
-import { useNavigate } from "react-router";
-import { NavLink } from "react-router-dom";
+
+import { logout } from "features/authentication/authSlice";
 
 const Sidebar = () => {
   const navigate = useNavigate();
+  const dispatch = useDispatch();
+
   const isActiveClass = ({ isActive }) =>
-    `flex items-center py-4 px-4 rounded-l w-full cursor-pointer ${
-      isActive && "py-[0.6rem] px-4  bg-deepBlue text-white"
+    `flex items-center py-4 px-4 w-full cursor-pointer ${
+      isActive && "py-[0.2rem] px-4 bg-activeGreen rounded-md"
     }`;
+
   return (
     <aside className="sidebar text-deepBlue text-2xl font-medium mt-5 cursor-pointer">
       <ul className="menu hidden sm:flex flex-col">
@@ -50,17 +58,21 @@ const Sidebar = () => {
             Liked Posts
           </NavLink>
         </li>
-        <li className="menu-item">
-          <NavLink exact="true" to="/logout" className={isActiveClass}>
-            <LogoutIcon style={{ fontSize: 35 }} className="mr-4" />
-            Log Out
-          </NavLink>
+        <li
+          className="menu-item flex items-center py-4 px-5"
+          onClick={() => {
+            dispatch(logout());
+            toast.success("Logged Out!");
+          }}
+        >
+          <LogoutIcon style={{ fontSize: 35 }} className="mr-4" />
+          Log Out
         </li>
 
         <li className="menu-item mt-10">
           <button
             onClick={() => navigate("/create-post")}
-            className="bg-customOrange w-full flex items-center py-2 px-4 text-white  rounded"
+            className="bg-customGreen w-full flex items-center py-2 px-5 text-white rounded"
           >
             <AddCircleOutlineOutlinedIcon
               style={{ fontSize: 35 }}
@@ -70,49 +82,35 @@ const Sidebar = () => {
           </button>
         </li>
       </ul>
+
       {/* -------------------- MenuBar For Mobile -------------------------- */}
       <div className="menu sm:hidden fixed bottom-0 inset-x-0 bg-deepBlue text-customOrange flex justify-between px-4 py-3">
-       <span  onClick={() => navigate("/explore")} className="flex flex-col items-center">
+        <span onClick={() => navigate("/explore")} className="flex flex-col items-center">
           <HomeOutlinedIcon style={{ fontSize: 35 }} className="text-white" />
         </span>
-        <span
-          onClick={() => navigate("/explore")}
-          className="flex flex-col items-center"
-        >
+        <span onClick={() => navigate("/explore")} className="flex flex-col items-center">
           <RocketOutlinedIcon style={{ fontSize: 35 }} className="text-white" />
         </span>
-        <span
-          onClick={() => navigate("/bookmarks")}
-          className="flex flex-col items-center"
-        >
-          <BookmarkBorderOutlinedIcon
-            style={{ fontSize: 35 }}
-            className="text-white"
-          />
+        <span onClick={() => navigate("/bookmarks")} className="flex flex-col items-center">
+          <BookmarkBorderOutlinedIcon style={{ fontSize: 35 }} className="text-white" />
+        </span>
+        <span onClick={() => navigate("/liked-posts")} className="flex flex-col items-center">
+          <FavoriteBorderOutlinedIcon style={{ fontSize: 35 }} className="text-white" />
         </span>
         <span
-          onClick={() => navigate("/liked-posts")}
+          onClick={() => {
+            dispatch(logout());
+            toast.success("Logged Out!");
+          }}
           className="flex flex-col items-center"
-        >
-          <FavoriteBorderOutlinedIcon
-            style={{ fontSize: 35 }}
-            className="text-white"
-          />
-        </span>
-
-        <span
-          onClick={() => navigate("/")}
-          className="flex flex-col items-cente"
         >
           <LogoutIcon style={{ fontSize: 35 }} className="text-white" />
         </span>
       </div>
+
       {/* Position the HistoryEduIcon above the right corner */}
-      <div className="absolute sm:hidden bottom-0 right-0 mb-[4.8rem] mr-[0.2rem] bg-customOrange rounded-full w-12 h-12 flex items-center justify-center">
-        <span
-          onClick={() => navigate("/create-post")}
-          className="text-white text-3xl"
-        >
+      <div className="absolute sm:hidden bottom-0 right-0 mb-[4.8rem] mr-[0.2rem] bg-customGreen rounded-full w-12 h-12 flex items-center justify-center">
+        <span onClick={() => navigate("/create-post")} className="text-white text-3xl">
           <HistoryEduIcon style={{ fontSize: 35 }} className="text-white" />
         </span>
       </div>
