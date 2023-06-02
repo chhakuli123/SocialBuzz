5ecdfdc6a5bbbf9fcc0c1f0881ef9d5125bacb7b
Chhakuli <chhakulizingare12@gmail.com>
2023-06-02 09:01:56 +0530

    feat: Add mockman for API testing

---
+++ src/components/shared/Mockman.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import Mockman from "mockman-js"
+
+function MockAPI() {
+  return (
+    <div className="MockAPI">
+        <Mockman/>
+    </div>
+  );
+}
+
+export { MockAPI };
