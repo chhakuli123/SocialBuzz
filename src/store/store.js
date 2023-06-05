63eda18af72dbc6454d6744cdd936fbe15a21574
Chhakuli <chhakulizingare12@gmail.com>
2023-06-05 10:20:16 +0530

    feat: Add post Slice

--- src/store/store.js
+++ src/store/store.js
@@ -1,9 +1,10 @@
 import { configureStore } from "@reduxjs/toolkit";
-import { authReducer, userReducer } from "slices";
+import { authReducer, postReducer, userReducer } from "slices";
 
 export const store = configureStore({
   reducer: {
     auth: authReducer,
     user: userReducer,
+    post: postReducer,
   },
 });
