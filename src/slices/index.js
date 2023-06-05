0fff039809525a9d84eb93ba9893a0182a4590da
Chhakuli <chhakulizingare12@gmail.com>
2023-06-05 10:09:44 +0530

    chore: Export of files

--- src/slices/index.js
+++ src/slices/index.js
@@ -1,2 +1,3 @@
 export { loginUser, signupUser, authReducer, logout } from "./authSlice";
 export { userReducer, fetchAllUsers } from "./userSlice";
+export { postReducer, getPosts } from "./postsSlice";
