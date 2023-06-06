40e475fbc6296374dfbca1b3896b48dba4bb990e
Chhakuli <chhakulizingare12@gmail.com>
2023-06-06 20:38:44 +0530

    feat: Sorted Post acording to newest

--- src/components/home/PostList.jsx
+++ src/components/home/PostList.jsx
@@ -1,54 +1,27 @@
-import React, { useState, useEffect, useRef } from "react";
+import React from "react";
 import { useSelector } from "react-redux";
 
 import { Post } from "./Post";
 import { PostForm } from "./PostForm";
 
-export const PostList = ({ posts }) => {
+const PostList = ({ posts }) => {
   const { allUsers } = useSelector((state) => state.user);
 
-  const [showOptions, setShowOptions] = useState(false);
-  const optionsRef = useRef(null);
-
-  useEffect(() => {
-    const handleClickOutside = (event) => {
-      if (
-        optionsRef.current &&
-        !optionsRef.current.contains(event.target) &&
-        !event.target.classList.contains("more-vert-icon")
-      ) {
-        setShowOptions(false);
-      }
-    };
-
-    document.addEventListener("mousedown", handleClickOutside);
-
-    return () => {
-      document.removeEventListener("mousedown", handleClickOutside);
-    };
-  }, []);
-
-  const handleOptionsClick = () => {
-    setShowOptions(!showOptions);
-  };
-
+  // Sort posts by createdAt in descending order
+  const sortedPosts = posts
+    ? [...posts].sort((a, b) => new Date(b.createdAt) - new Date(a.createdAt))
+    : [];
+    
   return (
     <div className="flex flex-col items-center justify-center w-full mt-5 mb-12 lg:mr-[5rem]">
       <PostForm />
-      {posts.map((post) => {
+      {sortedPosts.map((post) => {
         const user = allUsers.find((user) => user.username === post.username);
         return (
-          <Post
-            key={post._id}
-            post={post}
-            user={user}
-            allUsers={allUsers}
-            handleOptionsClick={handleOptionsClick}
-            optionsRef={optionsRef}
-            showOptions={showOptions}
-          />
+          <Post key={post._id} post={post} user={user} allUsers={allUsers} />
         );
       })}
     </div>
   );
 };
+export { PostList };
