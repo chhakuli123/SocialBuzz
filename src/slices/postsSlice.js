6c2e72b2869136e030470aaa64c2b10cc41efd12
Chhakuli <chhakulizingare12@gmail.com>
2023-06-06 20:20:00 +0530

    feat: Add cases for add,edit,delete

--- src/slices/postsSlice.js
+++ src/slices/postsSlice.js
@@ -1,14 +1,18 @@
 import { createAsyncThunk, createSlice } from "@reduxjs/toolkit";
+import { toast } from "react-hot-toast";
 
-import { getAllPostsFromServer } from "services";
+import { addPost, deletePost, editPost, getAllPostsFromServer } from "services";
 
 const initialState = {
   allPosts: [],
   getAllPostsStatus: "idle",
+  postAddStatus: "idle",
+  postDeleteStatus: "idle",
+  postEditStatus: "idle",
 };
 
 export const getPosts = createAsyncThunk(
-  "/post/getAllPosts",
+  "post/getAllPosts",
   async (_, { rejectWithValue }) => {
     try {
       const { data } = await getAllPostsFromServer();
@@ -16,6 +20,46 @@ export const getPosts = createAsyncThunk(
     } catch (error) {
       console.error(error.message);
       return rejectWithValue(error.message);
+    }
+  }
+);
+
+export const addUserPost = createAsyncThunk(
+  "post/addPost",
+  async (postData, { rejectWithValue }) => {
+    try {
+      const { data } = await addPost(postData);
+      return data.posts;
+    } catch (error) {
+      return rejectWithValue(error.message);
+    }
+  }
+);
+
+export const deleteUserPost = createAsyncThunk(
+  "post/deletePost",
+  async (postId, { rejectWithValue }) => {
+    try {
+      const response = await deletePost(postId);
+      if (response.posts) {
+        return response.posts;
+      } else {
+        throw new Error("Invalid response data");
+      }
+    } catch (e) {
+      return rejectWithValue(e.message);
+    }
+  }
+);
+
+export const editUserPost = createAsyncThunk(
+  "post/editPost",
+  async (postData, { rejectWithValue }) => {
+    try {
+      const { data } = await editPost(postData);
+      return data.posts;
+    } catch (e) {
+      return rejectWithValue(e.message);
     }
   }
 );
@@ -32,6 +76,30 @@ const postSlice = createSlice({
       })
       .addCase(getPosts.rejected, (state) => {
         state.getAllPostsStatus = "rejected";
+      })
+      .addCase(addUserPost.fulfilled, (state, action) => {
+        state.postAddStatus = "fulfilled";
+        state.allPosts = action.payload;
+        toast.success("Post Created Successfully!");
+      })
+      .addCase(addUserPost.rejected, (state) => {
+        state.postAddStatus = "rejected";
+      })
+      .addCase(deleteUserPost.fulfilled, (state, action) => {
+        state.postDeleteStatus = "fulfilled";
+        state.allPosts = action.payload;
+        toast.success("Post Deleted Successfully!");
+      })
+      .addCase(deleteUserPost.rejected, (state) => {
+        state.postDeleteStatus = "rejected";
+      })
+      .addCase(editUserPost.fulfilled, (state, action) => {
+        state.postEditStatus = "fulfilled";
+        state.allPosts = action.payload;
+        toast.success("Post Edited Successfully!");
+      })
+      .addCase(editUserPost.rejected, (state) => {
+        state.postEditStatus = "rejected";
       });
   },
 });
