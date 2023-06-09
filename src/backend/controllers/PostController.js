ba7df98f65d8d77e7d6b2c7352e427312bb77e7b
Chhakuli <chhakulizingare12@gmail.com>
2023-06-09 17:01:08 +0530

    refactor: Deep copy user object for post.likes.likedBy array

--- src/backend/controllers/PostController.js
+++ src/backend/controllers/PostController.js
@@ -1,6 +1,6 @@
 import { Response } from "miragejs";
-import { formatDate, requiresAuth } from "../utils/authUtils";
 import { v4 as uuid } from "uuid";
+import { formatDate, requiresAuth } from "../utils/authUtils";
 
 /**
  * All the routes related to post are present here.
@@ -181,7 +181,7 @@ export const likePostHandler = function (schema, request) {
       (currUser) => currUser._id !== user._id
     );
     post.likes.likeCount += 1;
-    post.likes.likedBy.push(user);
+    post.likes.likedBy.push(JSON.parse(JSON.stringify(user)));
     this.db.posts.update({ _id: postId }, { ...post, updatedAt: formatDate() });
     return new Response(201, {}, { posts: this.db.posts });
   } catch (error) {
@@ -234,7 +234,7 @@ export const dislikePostHandler = function (schema, request) {
     const updatedLikedBy = post.likes.likedBy.filter(
       (currUser) => currUser._id !== user._id
     );
-    post.likes.dislikedBy.push(user);
+    post.likes.dislikedBy.push(JSON.parse(JSON.stringify(user)));
     post = { ...post, likes: { ...post.likes, likedBy: updatedLikedBy } };
     this.db.posts.update({ _id: postId }, { ...post, updatedAt: formatDate() });
     return new Response(201, {}, { posts: this.db.posts });
