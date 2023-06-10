a93e10372cc21575e6875d3e31bc9cec520a57b4
Chhakuli <chhakulizingare12@gmail.com>
2023-06-10 10:18:50 +0530

    refactor: Made some changes

--- src/backend/controllers/UserController.js
+++ src/backend/controllers/UserController.js
@@ -20,9 +20,9 @@ export const getAllUsersHandler = function () {
  * */
 
 export const getUserHandler = function (schema, request) {
-  const userId = request.params.userId;
+  const username = request.params.username;
   try {
-    const user = schema.users.findBy({ _id: userId }).attrs;
+    const user = schema.users.findBy({ username: username }).attrs;
     return new Response(200, {}, { user });
   } catch (error) {
     return new Response(
